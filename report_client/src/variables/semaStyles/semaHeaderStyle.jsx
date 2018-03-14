a1cf11fe9299fac986ef19a3e158de7521bd9c12
fredoleary <fred@essentel.com>
2018-03-14 16:00:11 -0700

    Adding header div

---
+++ report_client/src/variables/semaStyles/semaHeaderStyle.jsx
@@ -0,0 +1,15 @@
+// ##############################
+// // // Sidebar styles
+// #############################
+
+import {semaBackgroundColor} from "variables/styles";
+
+const semaHeaderStyle = theme => ({
+    fooStyle : {
+        backgroundColor: semaBackgroundColor,
+        height: '20%'
+    }
+
+});
+
+export default semaHeaderStyle;
