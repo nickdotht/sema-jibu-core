24e667077879e4975cbf801d8465e9ea3f965b54
fredoleary <fred@essentel.com>
2018-03-15 15:12:41 -0700

    Cleanup redux flow

--- report_client/src/actions.js
+++ report_client/src/actions.js
@@ -1,19 +1,6 @@
 import * as types from 'actions/actionTypes';
 
 
-export function receiveStuff(json) {
-    var array = json.map( name => name.username)
-    return {type: types.RECEIVE_SEMA_USER, stuff: array};
-}
-
-export function fetchStuff() {
-    return dispatch => {
-        return fetch('/users')
-        .then(response => response.json())
-        .then(json => dispatch(receiveStuff(json)));
-    };
-}
-
 export function receiveSeamaUser(json) {
     var name = json.seamaUser
     return {type: types.RECEIVE_SEMA_USER, seamaUser: name};
@@ -26,9 +13,4 @@ export function fetchSeamaUser() {
             .then(json => dispatch(receiveSeamaUser(json)));
     };
 }
-
-// export function fetchSeamaUser() {
-//     return {type: types.RECEIVE_SEMA_USER, seamaUser: "Whiskey"};
-// }
-
 
--- report_client/src/reducers/seamaUserReducer.js
+++ report_client/src/reducers/seamaUserReducer.js
@@ -1,12 +1,9 @@
 import initialState from './initialState';
-import {FETCH_STUFF, RECEIVE_SEMA_USER} from 'actions/actionTypes';
+import {RECEIVE_SEMA_USER} from 'actions/actionTypes';
 
 export default function seamaUser(state = initialState.seamaUser, action) {
     let newState;
     switch (action.type) {
-        case FETCH_STUFF:
-            console.log('FETCH_STUFF Action')
-            return action;
         case RECEIVE_SEMA_USER:
             newState = action.seamaUser;
             console.log('RECEIVE_SEMA_USER Action')
