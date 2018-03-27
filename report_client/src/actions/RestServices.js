add29bd3ffcf15669c58a20a18e2d064c425a42c
fredoleary <fred@essentel.com>
2018-03-26 18:37:24 -0700

    Added logic to read kiosks

--- report_client/src/actions/RestServices.js
+++ report_client/src/actions/RestServices.js
@@ -12,15 +12,26 @@ export function receiveSeamaUser(json) {
 }
 
 export function fetchSeamaUser() {
-    try {
-        return fetch('/seama_user')
-            .then(response => response.json())
-            .then(json => receiveSeamaUser(json))
-            .catch(function(error){
-                console.log("fetchSeamaUser failed", error);
-            });
-    }catch (ex){
-        console.log(ex)
-    }
+    return fetch('/seama_user')
+        .then(response => response.json())
+        .then(json => receiveSeamaUser(json))
+        .catch(function(error){
+            console.log("fetchSeamaUser failed", error);
+        });
+}
+
+export function receiveSeamaKiosks(json) {
+    var kiosk = json.kiosks;
+    rootComponent.setState( {seamaKiosk:kiosk});
+    console.log("receiveSeamaUser - ", kiosk)
+}
+
+export function fetchSeamaKiosks() {
+    return fetch('/seama_kiosks')
+        .then(response => response.json())
+        .then(json => receiveSeamaKiosks(json))
+        .catch(function(error){
+            console.log("fetchSeamaKiosks failed", error);
+        });
 }
 
--- report_client/src/components/SeamaToolbar.js
+++ report_client/src/components/SeamaToolbar.js
@@ -12,6 +12,7 @@ class SeamaToolbar extends Component {
         super(props, context);
 
         this.handleSelect = this.handleSelect.bind(this);
+        this.buildMenuItems = this.buildMenuItems.bind(this);
 
         this.state = {
             title: "fooyxxx",
@@ -33,14 +34,28 @@ class SeamaToolbar extends Component {
     }
 
     componentDidMount() {
-        RestServices.fetchSeamaUser('/users');
+        RestServices.fetchSeamaUser();
+        RestServices.fetchSeamaKiosks();
     }
 
     handleSelect(eventKey, foo){
-        console.log(eventKey, this.state.Menu[eventKey]);
-        this.setState({title: this.state.Menu[eventKey]});
+        console.log(eventKey, this.props.seamaState.seamaKiosk[eventKey].name);
+        this.setState({title: this.props.seamaState.seamaKiosk[eventKey].name});
     };
 
+    buildMenuItems(){
+        console.log("whiskey")
+        var menuItems = [];
+        if( this.props.seamaState.seamaKiosk){
+            var keys = Object.keys(this.props.seamaState.seamaKiosk);
+            for( var i = 0; i < keys.length; i++ ){
+                var kiosk = this.props.seamaState.seamaKiosk[keys[i]];
+                console.log( kiosk);
+                menuItems.push(<MenuItem eventKey={keys[i]}style={menuStyle}>{kiosk.name}</MenuItem>);
+            }
+        }
+        return menuItems;
+    }
     render() {
         return (
             <div>
@@ -59,7 +74,8 @@ class SeamaToolbar extends Component {
                             Linkcc
                         </NavItem>
                         <NavDropdown eventKey={3} title={this.state.title} onSelect={this.handleSelect} id="basic-nav-dropdown" >
-                            {this.menuItems}
+                            {this.buildMenuItems()}
+                            {/*{this.menuItems}*/}
                             {/*<MenuItem eventKey={3.1} style={menuStyle}>{this.state.Menu["3.1"]}</MenuItem>*/}
                             {/*<MenuItem eventKey={3.2} style={menuStyle}>{this.state.Menu["3.2"]}</MenuItem>*/}
                             {/*<MenuItem eventKey={3.3} style={menuStyle}>{this.state.Menu["3.3"]}</MenuItem>*/}
