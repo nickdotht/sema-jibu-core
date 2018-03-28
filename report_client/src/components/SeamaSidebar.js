908d430117217e6c3df5ca5dce2412dce6b33bf6
fredoleary <fred@essentel.com>
2018-03-28 12:42:44 -0700

    Cleanup

--- report_client/src/components/SeamaSidebar.js
+++ report_client/src/components/SeamaSidebar.js
@@ -15,14 +15,14 @@ const SeamaSidebarStyle = {
 
 const SeamaListStyleActive = {
     textAlign:"left",
-    paddingLeft:"30px",
+    paddingLeft:"20px",
     backgroundColor:"rgb(24,55,106)",
     color:"white"
 };
 
 const SeamaListStyleInactive = {
     textAlign:"left",
-    paddingLeft:"30px",
+    paddingLeft:"20px",
     color:"rgba(255,255,255,.7)",
     backgroundColor:"rgb(24,55,106)"
 };
@@ -30,8 +30,10 @@ const SeamaListStyleInactive = {
 const ImageStyle = {
     resize:"both",
     width:"249px",
-    height:"80px"
+    height:"80px",
 };
+
+
 function CustomComponent1({ children }) {
     return (
         <li className="list-group-item" onClick={() => {}} style={SeamaListStyleActive}>
@@ -55,12 +57,12 @@ class SeamaSidebar extends Component {
                     {<img src={require('images/sema-sidebar-logo.png')} alt="logo" style={ImageStyle} />}
                 </div>
                 <ListGroup>
-                    <CustomComponent1 href="#" active style={SeamaListStyleActive}><i class="glyphicon glyphicon-map-marker"></i>  Water Operations</CustomComponent1>
-                    <CustomComponent2 href="#" active style={SeamaListStyleInactive}><i class="	glyphicon glyphicon-shopping-cart"></i>  Sales</CustomComponent2>
-                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-globe"></i>  Distribution Map</CustomComponent2>
-                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-time"></i>  Delivery Schedule</CustomComponent2>
-                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-inbox"></i>  Inventory Management</CustomComponent2>
-                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-usd"></i>  Financials</CustomComponent2>
+                    <CustomComponent1 href="#" active style={SeamaListStyleActive}><i class="glyphicon glyphicon-map-marker" style={{paddingRight:"15px"}}></i>Water Operations</CustomComponent1>
+                    <CustomComponent2 href="#" active style={SeamaListStyleInactive}><i class="	glyphicon glyphicon-shopping-cart" style={{paddingRight:"15px"}}></i>Sales</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-globe" style={{paddingRight:"15px"}}></i>Distribution Map</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-time" style={{paddingRight:"15px"}}></i>Delivery Schedule</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-inbox" style={{paddingRight:"15px"}}></i>Inventory Management</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-usd" style={{paddingRight:"15px"}}></i>Financials</CustomComponent2>
                 </ListGroup>;
             </div>
         );
