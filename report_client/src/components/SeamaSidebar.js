9a023b74300ba2368790c9d65b218e9d709aac07
fredoleary <fred@essentel.com>
2018-03-28 12:18:54 -0700

    Added sidebar menu

--- report_client/src/components/SeamaSidebar.js
+++ report_client/src/components/SeamaSidebar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, Jumbotron, Button, Nav,NavItem,NavDropdown,MenuItem } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
 import 'App.css';
 
-var SeamaSidebarStyle = {
-    background:"#f8f8f8",
+const SeamaSidebarStyle = {
+    background:"rgb(24,55,106)",
     position:"fixed",
     width:"250px",
     height:"100%",
@@ -11,13 +11,57 @@ var SeamaSidebarStyle = {
     borderRight:"1px",
     borderRightColor:"#e0e0e0",
     borderRightStyle:"solid"
-}
+};
 
+const SeamaListStyleActive = {
+    textAlign:"left",
+    paddingLeft:"30px",
+    backgroundColor:"rgb(24,55,106)",
+    color:"white"
+};
+
+const SeamaListStyleInactive = {
+    textAlign:"left",
+    paddingLeft:"30px",
+    color:"rgba(255,255,255,.7)",
+    backgroundColor:"rgb(24,55,106)"
+};
+
+const ImageStyle = {
+    resize:"both",
+    width:"249px",
+    height:"80px"
+};
+function CustomComponent1({ children }) {
+    return (
+        <li className="list-group-item" onClick={() => {}} style={SeamaListStyleActive}>
+            {children}
+        </li>
+    );
+}
+function CustomComponent2({ children }) {
+    return (
+        <li className="list-group-item" onClick={() => {}} style={SeamaListStyleInactive}>
+            {children}
+        </li>
+    );
+}
 
 class SeamaSidebar extends Component {
     render() {
         return (
             <div style={SeamaSidebarStyle}>
+                <div>
+                    {<img src={require('images/sema-sidebar-logo.png')} alt="logo" style={ImageStyle} />}
+                </div>
+                <ListGroup>
+                    <CustomComponent1 href="#" active style={SeamaListStyleActive}><i class="glyphicon glyphicon-map-marker"></i>  Water Operations</CustomComponent1>
+                    <CustomComponent2 href="#" active style={SeamaListStyleInactive}><i class="	glyphicon glyphicon-shopping-cart"></i>  Sales</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-globe"></i>  Distribution Map</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-time"></i>  Delivery Schedule</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-inbox"></i>  Inventory Management</CustomComponent2>
+                    <CustomComponent2 href="#" disabled style={SeamaListStyleInactive}><i class="glyphicon glyphicon-usd"></i>  Financials</CustomComponent2>
+                </ListGroup>;
             </div>
         );
     }
