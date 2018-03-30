19b92f84bf11a660b8d80bcfae7798810a52a46a
fredoleary <fred@essentel.com>
2018-03-30 14:11:39 -0700

    Position using newer grids...

--- report_client/src/components/SeamaWaterQualityContainer.js
+++ report_client/src/components/SeamaWaterQualityContainer.js
@@ -1,40 +1,12 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
 import 'App.css';
 import SeamaSummaryPanel1 from "./SeamaSummaryPanel1";
 import SeamaWaterOperationsChart from "./SeamaWaterOperationsChart";
 import SeamaSidebar from "./SeamaSidebar";
-
-var SeamaWaterQualityContainerStyle = {
-    background:"#f0fff0",
-    position:"fixed",
-    width:"100%",
-    height:"100%",
-    // top:"52px",
-//    left:"250px"
-}
-
-var SeamaGridStyle = {
-     width:"100%"
-}
-
-const colFixed240 = {
-    width:"240px",
-    background:"rgb(255,0,0)",
-    position:"fixed",
-    height:"100%",
-    zIndex:"1"
-}
+import 'css/Seama.css';
 
 
-const colOffset400 ={
-    paddingLeft:"240px",
-    paddingRight:"0",
-    background:"(0,255,0)",
-    position:"fixed",
-    height:"100%",
-    zIndex:"0"
-}
+
 class SeamaWaterQualityContainer extends Component {
     constructor(props, context) {
         super(props, context);
@@ -43,35 +15,55 @@ class SeamaWaterQualityContainer extends Component {
 
     render() {
         return (
-            <div className="container" style={{marginLeft:0, marginRight:0}}>
-                <div className="row">
-                    <div style={colFixed240}>
-                        <SeamaSidebar/>
+            <div className="SeamaGridContainer">
+                <div className ="SeamaSidebar">
+                    <SeamaSidebar/>
+                </div>
+                <div className = "SeamaSummaryContainer">
+                    <div className ="SeamaWaterQualitySummary">
+                        <SeamaSummaryPanel1 title="Total Production" units={"Gallons"} value={this.props.seamaState.seamaWaterQuality["totalProduction"]}/>
                     </div>
-                    {/*<div style={colFixed160}>Fixed 160px</div>*/}
-                    <div className="col-md-12" style={colOffset400}>
-                        <div className="row">
-                            <Grid style={SeamaGridStyle}>
-                                <Row className="show-grid" style={{height:"30vh", background:"rgb(171,193,222"}}>
-                                    <Col xs={4} md={4} style={{height:"100%", display:"table"}} >
-                                        <SeamaSummaryPanel1 title="Total Production" units={"Gallons"} value={this.props.seamaState.seamaWaterQuality["totalProduction"]}></SeamaSummaryPanel1>
-                                    </Col>
-                                    <Col xs={4} md={4} style={{height:"100%", display:"table"}}>
-                                        <SeamaSummaryPanel1 title="Site Pressure" units={"PSI"} value={this.props.seamaState.seamaWaterQuality["sitePressure"]}></SeamaSummaryPanel1>
-                                    </Col>
-                                    <Col xs={4} md={4} style={{height:"100%", display:"table"}}>
-                                        <SeamaSummaryPanel1 title="Flow Rate" units={"GPM"} value={this.props.seamaState.seamaWaterQuality["flowRate"]}></SeamaSummaryPanel1>
-                                    </Col>
-                                </Row>
-                                <Row className="show-grid" style={{background:"white", height:"70vh"}}>
-                                    <Col xs={12} md={12} style={{height:"100%"}}>
-                                        <SeamaWaterOperationsChart chartData={this.props.seamaState.seamaWaterQuality["production"]}></SeamaWaterOperationsChart>
-                                    </Col>
-                                </Row>
-                            </Grid>
-                        </div>
+                    <div className ="SeamaWaterQualitySummary">
+                        <SeamaSummaryPanel1 title="Site Pressure" units={"PSI"} value={this.props.seamaState.seamaWaterQuality["sitePressure"]}/>
+                    </div>
+                    <div className ="SeamaWaterQualitySummary">
+                        <SeamaSummaryPanel1 title="Flow Rate" units={"GPM"} value={this.props.seamaState.seamaWaterQuality["flowRate"]}/>
                     </div>
                 </div>
+                <div className = "SeamaChartContainer">
+                    <div className= "SeamaWaterQualityMainChart">
+                         <SeamaWaterOperationsChart chartData={this.props.seamaState.seamaWaterQuality["production"]}/>
+                    </div>
+                    <div className= "SeamaWaterQualitySecondaryChart1">
+                        <SeamaWaterOperationsChart chartData={this.props.seamaState.seamaWaterQuality["production"]}/>
+                    </div>
+                    <div className= "SeamaWaterQualitySecondaryChart2">
+                        <SeamaWaterOperationsChart chartData={this.props.seamaState.seamaWaterQuality["production"]}/>
+                    </div>
+                </div>
+
+                {/*<div style={colFixed160}>Fixed 160px</div>*/}
+                {/*<div className="col-md-12" style={colOffset400}>*/}
+                    {/*<div className="row">*/}
+                        {/*<Grid style={SeamaGridStyle}>*/}
+                            {/*<Row className="show-grid" style={{height:"30vh", background:"rgb(171,193,222"}}>*/}
+                                {/*<Col xs={4} md={4} style={{height:"100%", display:"table"}} >*/}
+                                    {/*<SeamaSummaryPanel1 title="Total Production" units={"Gallons"} value={this.props.seamaState.seamaWaterQuality["totalProduction"]}></SeamaSummaryPanel1>*/}
+                                {/*</Col>*/}
+                                {/*<Col xs={4} md={4} style={{height:"100%", display:"table"}}>*/}
+                                {/*</Col>*/}
+                                {/*<Col xs={4} md={4} style={{height:"100%", display:"table"}}>*/}
+                                    {/*<SeamaSummaryPanel1 title="Flow Rate" units={"GPM"} value={this.props.seamaState.seamaWaterQuality["flowRate"]}></SeamaSummaryPanel1>*/}
+                                {/*</Col>*/}
+                            {/*</Row>*/}
+                            {/*<Row className="show-grid" style={{background:"white", height:"70vh"}}>*/}
+                                {/*<Col xs={12} md={12} style={{height:"100%"}}>*/}
+                                    {/*<SeamaWaterOperationsChart chartData={this.props.seamaState.seamaWaterQuality["production"]}></SeamaWaterOperationsChart>*/}
+                                {/*</Col>*/}
+                            {/*</Row>*/}
+                        {/*</Grid>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
             </div>
         );
     }
