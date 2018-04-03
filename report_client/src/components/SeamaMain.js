9279ad5288108e7c54f60c8eb51b7fde0981ee17
fredoleary <fred@essentel.com>
2018-04-02 20:08:08 -0700

    Refactoring...

--- report_client/src/components/SeamaMain.js
+++ report_client/src/components/SeamaMain.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import SeamaWaterQualityContainer from "./SeamaWaterQuality";
+import SeamaWaterQuality from "./SeamaWaterQuality";
 import SeamaSales from "./SeamaSales";
 import SeamaDistributionMap from "./SeamaDistributionMap";
 import SeamaDeliverySchedule from "./SeamaDeliverySchedule";
@@ -10,7 +10,7 @@ import SeamaFinancials from "./SeamaFinancials";
 const Main = (props) => (
     <main>
         <Switch>
-            <Route exact path='/' render={(routeProps) =>(<SeamaWaterQualityContainer {...routeProps } {...props} /> )}/>
+            <Route exact path='/' render={(routeProps) =>(<SeamaWaterQuality {...routeProps } {...props} /> )}/>
             <Route path='/Sales' render={(routeProps) =>(<SeamaSales {...routeProps } {...props} /> )}/>
             <Route path='/DistributionMap' render={(routeProps) =>(<SeamaDistributionMap {...routeProps } {...props} /> )}/>
             <Route path='/DeliverySchedule' render={(routeProps) =>(<SeamaDeliverySchedule {...routeProps } {...props} /> )}/>
