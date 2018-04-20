02382fe1df79bd0022aced3b5b2475bec76f7abb
Nick Rameau <r@meau.me>
2018-04-20 22:16:58 +0000

    Use absolute path from base dir to render the frontend

--- report_server/routes/index.js
+++ report_server/routes/index.js
@@ -4,10 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log( "path", path.join(__dirname+'/build/index.html'));
-//    res.render('index', { title: 'Express Fred' });
-//    res.sendFile(path.join(__dirname+'/build/index.html'));
-    res.sendFile('build/index.html');
+	res.sendFile(`${__basedir}/public_react/build/index.html`);
 });
 
 module.exports = router;
