9d790a7351de90cb88735af74b7fbcde182e51d0
fredoleary <fred@essentel.com>
2018-05-08 10:10:22 -0700

    DLO1-6	Server-Implement Sales REST service Implement sales by retailer

--- report_server/routes/sema_sales.js
+++ report_server/routes/sema_sales.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var connectionTable = require('../seama_services/db_service').connectionTable;
+const express = require('express');
+const router = express.Router();
+const connectionTable = require('../seama_services/db_service').connectionTable;
 
 /* GET data for sales view. */
 const sqlTotalCustomers =
@@ -27,13 +27,22 @@ const sqlLitersPerCustomer =
 	GROUP BY  YEAR(created_date), MONTH(created_date)\
 	ORDER BY DATE(created_date) DESC';
 
+// TODO This query isn't correct. It does NOT resolve retailers
 const sqlRetailers =
 	'SELECT * \
 	FROM customer_account \
 	INNER JOIN sales_channel_customer_accounts \
 	ON customer_account.id = sales_channel_customer_accounts.customer_account_id \
-	WHERE customer_account.kiosk_id = ?';
+	WHERE customer_account.kiosk_id = ? AND customer_account.gps_coordinates != ""';
 
+const sqlRetailerRevenue =
+	'SELECT SUM(customer_amount), created_date, customer_account_id, customer_account.contact_name \
+	FROM receipt \
+	INNER JOIN customer_account \
+	ON customer_account_id = customer_account.id \
+	WHERE customer_account.kiosk_id = ? AND datediff(curdate(), created_date) < 70 AND customer_account_id in( ? )\
+	GROUP BY  YEAR(created_date), MONTH(created_date), customer_account_id \
+	ORDER BY customer_account_id, created_date DESC';
 
 router.get('/', function(request, response) {
 	console.log('Sales - ', request.query.kioskID);
@@ -46,7 +55,11 @@ router.get('/', function(request, response) {
 			getRevenueByPeriod(connection, request.query, results).then(() => {
 				getLitersPerCustomer(connection, request.query, results).then(() => {
 					getRetailers(connection, request.query, results).then(() => {
-						yieldResults(response, results);
+						getRetailerRevenue(connection, request.query, results).then(() => {
+							yieldResults(response, results);
+						}).catch(err => {
+							yieldError(err, response, 500, results);
+						})
 					}).catch(err => {
 						yieldError(err, response, 500, results);
 					})
@@ -141,12 +154,13 @@ const getRetailers = (connection, requestParams, results ) => {
 			} else {
 				if (Array.isArray(sqlResult) && sqlResult.length >= 1) {
 					let retailers = [];
+					let period = (requestParams.period) ? requestParams.period : "month";
 					sqlResult.forEach(row => {
 						let retailer = {
 							name: row.contact_name,
 							id: row.id,
 							total: "N/A",
-							period: requestParams.period,
+							period: period,
 							thisPeriod: "N/A",
 							lastPeriod: "N/A",
 							gps: row.gps_coordinates
@@ -161,6 +175,44 @@ const getRetailers = (connection, requestParams, results ) => {
 	});
 };
 
+const getRetailerRevenue = (connection, requestParams, results ) => {
+	return new Promise((resolve, reject) => {
+		let inSet = results.retailSales.map(retailer => {return retailer.id});
+
+		connection.query(sqlRetailerRevenue, [requestParams.kioskID, inSet], function (err, sqlResult ) {
+			if (err) {
+				reject(err);
+			} else {
+				if (Array.isArray(sqlResult) && sqlResult.length > 0) {
+					inSet.forEach(retailerId => {
+						updateSales(retailerId, results.retailSales, sqlResult)
+					});
+				}
+				resolve();
+			}
+		});
+	});
+};
+
+const updateSales = ( retailer_id, retailers, sqlResults ) =>{
+	let sqlIndex = sqlResults.findIndex(sqlResult => sqlResult.customer_account_id === retailer_id);
+	const retailerIndex = retailers.findIndex(retailer => retailer.id === retailer_id);
+	// Sql Index = index into sales result for the retailer, retailerIndex is the object to populate
+	if(sqlIndex !== -1 &&  retailerIndex !== -1) {
+		// Do NOT use current month. It may not be complete.
+		let nowDate = new Date(Date.now());
+		if (nowDate.getFullYear() === sqlResults[sqlIndex].created_date.getFullYear() &&
+			nowDate.getMonth() === sqlResults[sqlIndex].created_date.getMonth()) {
+			sqlIndex += 1;
+		}
+		if ((sqlIndex + 1) < sqlResults.length && // There must be two or more periods
+			sqlResults[sqlIndex].customer_account_id === retailer_id &&		// And the next two belong to the this retailer
+			sqlResults[sqlIndex + 1].customer_account_id === retailer_id) {
+			retailers[retailerIndex].thisPeriod = sqlResults[sqlIndex]["SUM(customer_amount)"];
+			retailers[retailerIndex].lastPeriod = sqlResults[sqlIndex+1]["SUM(customer_amount)"];
+		}
+	}
+};
 
 const yieldResults =(res, results ) =>{
 	res.json(results);
