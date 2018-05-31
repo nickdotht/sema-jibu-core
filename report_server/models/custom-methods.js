43de63c7fcf3d91764b85d884b62c161de87820c
Nick Rameau <r@meau.me>
2018-05-30 20:00:29 -0400

    Add custom associations and methods and ignore them in the models folder

---
+++ report_server/models/custom-methods.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt');
+
+module.exports = models => {
+	// Will execute everytime a user gets created or modified and the password has been changed
+	models.user.beforeSave(async (user, options) => {
+		if (user.changed('password')) return;
+
+		try {
+			user.password = await bcrypt.hashSync(user.password, process.env.BCRYPT_SALT_ROUNDS);
+		} catch (err) {
+			console.error(err);
+		}
+	});
+
+	// Instance level method: to use when comparing passwords on user login
+	models.user.prototype.comparePassword = () =>
+		bcrypt.compareSync(pw, this.password);
+};
--- report_server/models/index.js
+++ report_server/models/index.js
@@ -1,28 +1,33 @@
 'use strict';
 
-const fs        = require('fs');
-const path      = require('path');
-const Sequelize = require('sequelize');
-const basename  = path.basename(__filename);
-const db        = {};
+const fs			= require('fs');
+const path			= require('path');
+const Sequelize		= require('sequelize');
+const basename		= path.basename(__filename);
+const associations	= require('./associations');
+const customMethods	= require('./custom-methods');
+const db			= {};
 
-const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD);
+const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOST,
+	dialect: process.env.DB_DIALECT
+});
 
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+	  return (file.indexOf('.') !== 0) &&
+		  (file !== basename) &&
+		  (!['associations.js', 'custom-methods.js'].includes(file)) &&
+		  (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
+	const model = sequelize['import'](path.join(__dirname, file));
+	db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+customMethods(db);
+associations(db);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
