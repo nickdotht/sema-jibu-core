25d0b246e8019f570afd783b94a5168630ec7c8f
fredoleary <fred@essentel.com>
2018-06-01 16:57:33 -0700

    Order items complete calculator functions

--- jibu_pos/src/actions/OrderActions.js
+++ jibu_pos/src/actions/OrderActions.js
@@ -1,8 +1,20 @@
 export const ADD_PRODUCT_TO_ORDER = 'ADD_PRODUCT';
 export const CLEAR_ORDER = 'CLEAR_ORDER';
+export const REMOVE_PRODUCT = 'REMOVE_PRODUCT';
+export const SET_PRODUCT_QUANTITY = 'SET_PRODUCT_QUANTITY';
 
 export function AddProductToOrder( product, quantity ) {
 	console.log("AddProductToOrder - action");
-
 	return (dispatch) => { dispatch({type: ADD_PRODUCT_TO_ORDER, data:{product:product, quantity:quantity}})};
 }
+
+export function RemoveProductFromOrder( product ) {
+	console.log("RemoveProductFromOrder - action");
+	return (dispatch) => { dispatch({type: REMOVE_PRODUCT, data:{product:product}})};
+}
+
+export function SetProductQuantity( product, quantity ) {
+	console.log("SetProductQuantity - action");
+	return (dispatch) => { dispatch({type: SET_PRODUCT_QUANTITY, data:{product:product, quantity:quantity}})};
+}
+
--- jibu_pos/src/components/orders/OrderItems.js
+++ jibu_pos/src/components/orders/OrderItems.js
@@ -4,13 +4,30 @@ import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import * as OrderActions from "../../actions/OrderActions";
 
-const widthQuanityModal = 400;
-const heightQuanityModal = 500;
+const widthQuanityModal = 250;
+const heightQuanityModal = 400;
 
+const calculatorDigits = [
+	{id:7, display:"7"},
+	{id:8, display:"8"},
+	{id:9, display:"9"},
+	{id:4, display:"4"},
+	{id:5, display:"5"},
+	{id:6, display:"6"},
+	{id:1, display:"1"},
+	{id:2, display:"2"},
+	{id:3, display:"3"},
+	{id:0, display:"0"},
+	{id:99, display:"CLEAR"},
+];
 class OrderItems extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {isQuantityVisible :false, selectedItem:{}};
+		this.state = {
+			isQuantityVisible :false,
+			selectedItem:{},
+			accumulator:0,
+			firstKey:true};
 	}
 
 	render() {
@@ -45,16 +62,10 @@ class OrderItems extends Component {
 	onPressItem = (item) =>{
 		this.setState( {isQuantityVisible:true} );
 		this.setState( {selectedItem:item});
+		this.setState( {accumulator: item.quantity});
+		this.setState( {firstKey:true});
 	};
 
-	ShowQuantityContent = (item) => (
-		<View style={styles.quantityModal}>
-			<View style={styles.modalTotal }>
-				<Text style={ { flex:1, textAlign: 'center'}}>{this.state.selectedItem.quantity}</Text>
-			</View>
-			{/*{this._renderButton('Close', () => this.setState({ visibleModal: null }))}*/}
-		</View>
-	);
 
 
 	getRow = (item) =>{
@@ -74,7 +85,7 @@ class OrderItems extends Component {
 				</View>
 			</View>
 		);
-	}
+	};
 	showHeader = () =>{
 		return (
 			<View style={[{flex: 1, flexDirection: 'row'},styles.headerBackground]}>
@@ -89,6 +100,69 @@ class OrderItems extends Component {
 				</View>
 			</View>
 		);
+	};
+
+	ShowQuantityContent = (item) => (
+		<View style={styles.quantityModal}>
+			<View style={styles.modalTotal }>
+				<Text style={styles.accumulator}>{this.state.accumulator}</Text>
+			</View>
+			<View style={styles.modalCalculator }>
+				<View style={{flex:1}}>
+					<FlatList
+						data={calculatorDigits}
+						renderItem={({item, index, separators}) => (
+							<TouchableHighlight
+								onPress={() => this.onDigit(item)}
+								onShowUnderlay={separators.highlight}
+								onHideUnderlay={separators.unhighlight}>
+								{this.getDigit(item)}
+							</TouchableHighlight>
+						)}
+						keyExtractor={item => item.id}
+						numColumns={3}
+					/>
+
+				</View>
+			</View>
+
+			<View style={styles.modalDone }>
+				<TouchableHighlight style ={{flex:1}}
+				onPress={() => this.onDone()}>
+					<Text style={styles.doneButton}>Done</Text>
+				</TouchableHighlight>
+			</View>
+		</View>
+	);
+
+	onDigit = (digit) =>{
+		if( digit.id === 99 ){
+			this.setState({accumulator:0});
+		}else if( this.state.firstKey ){
+			this.setState({firstKey:false});
+			this.setState( {accumulator:digit.id});
+		}else{
+			this.setState( {accumulator: (this.state.accumulator *10) + digit.id});
+		}
+	};
+	getDigit = (digit) =>{
+		return (
+			<View style={this.getDigitStyle(digit)}>
+				<Text style={styles.digit} >{digit.display}</Text>
+			</View>
+		)
+	};
+	getDigitStyle = (digit) =>{
+		return (digit.id === 99 ) ? styles.clearContainer : styles.digitContainer;
+	};
+
+	onDone = ()=>{
+		this.setState( {isQuantityVisible:false} );
+		if( this.state.accumulator ===0  ){
+			this.props.orderActions.RemoveProductFromOrder( this.state.selectedItem.product );
+		}else{
+			this.props.orderActions.SetProductQuantity( this.state.selectedItem.product, this.state.accumulator );
+		}
 	};
 }
 
@@ -137,8 +211,8 @@ const styles = StyleSheet.create({
 		width:widthQuanityModal,
 		height:heightQuanityModal,
 		position: 'absolute',
-		bottom:80,
-		right:30,
+		bottom:120,
+		right:100,
 		backgroundColor: '#e0e0e0',
 		// padding: 22,
 		// justifyContent: 'center',
@@ -155,6 +229,50 @@ const styles = StyleSheet.create({
 		borderColor:'black',
 		borderWidth:4,
 		borderRadius:3
+	},
+	modalCalculator: {
+		flex: .70,
+		flexDirection: 'row',
+	},
+	modalDone: {
+		flex: .15,
+		backgroundColor:'#2858a7',
+		flexDirection: 'row',
+		alignItems:'center',
 
-}
+	},
+	digitContainer:{
+		flex: 1,
+		width:widthQuanityModal/3,
+		height:(.7*heightQuanityModal)/4,
+		alignItems:'center',
+		justifyContent: 'center'
+	},
+	clearContainer:{
+		flex: 1,
+		width:widthQuanityModal*2/3,
+		height:(.7*heightQuanityModal)/4,
+		alignItems:'center',
+		justifyContent: 'center'
+	},
+	digit: {
+		textAlign: 'center',
+		color: 'black',
+		fontWeight: 'bold',
+		fontSize: 30,
+	},
+	accumulator:{
+		color: 'black',
+		fontWeight: 'bold',
+		fontSize: 30,
+		flex:1,
+		textAlign: 'center'
+	},
+	doneButton: {
+		color: 'white',
+		fontWeight: 'bold',
+		fontSize: 30,
+		flex: 1,
+		textAlign: 'center'
+	}
 });
