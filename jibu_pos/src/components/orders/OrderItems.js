4aa201439d90fe9387a7794ad856dbec60bd1b4f
fredoleary <fred@essentel.com>
2018-06-01 12:18:31 -0700

    Order items update modal dialog

--- jibu_pos/src/components/orders/OrderItems.js
+++ jibu_pos/src/components/orders/OrderItems.js
@@ -1,10 +1,18 @@
 import React, {Component}  from "react"
-import { View, Text, FlatList, TouchableHighlight, StyleSheet } from "react-native";
+import { View, Modal, Text, FlatList, TouchableHighlight, StyleSheet } from "react-native";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import * as OrderActions from "../../actions/OrderActions";
 
+const widthQuanityModal = 400;
+const heightQuanityModal = 500;
+
 class OrderItems extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {isQuantityVisible :false, selectedItem:{}};
+	}
+
 	render() {
 		return (
 			<View style = {styles.container}>
@@ -22,10 +30,31 @@ class OrderItems extends Component {
 					)}
 					keyExtractor={item => item.product.id}
 				/>
-
+				<Modal visible = {this.state.isQuantityVisible}
+					   backdropColor={'red'}
+					   transparent ={true}
+					   onRequestClose ={this.closeHandler}>
+					{this.ShowQuantityContent()}
+				</Modal>
 			</View>
 		);
 	}
+	closeHandler = () =>{
+		this.setState( {isQuantityVisible:false} );
+	}
+	onPressItem = (item) =>{
+		this.setState( {isQuantityVisible:true} );
+		this.setState( {selectedItem:item});
+	};
+
+	ShowQuantityContent = (item) => (
+		<View style={styles.quantityModal}>
+			<View style={styles.modalTotal }>
+				<Text style={ { flex:1, textAlign: 'center'}}>{this.state.selectedItem.quantity}</Text>
+			</View>
+			{/*{this._renderButton('Close', () => this.setState({ visibleModal: null }))}*/}
+		</View>
+	);
 
 
 	getRow = (item) =>{
@@ -103,6 +132,29 @@ const styles = StyleSheet.create({
 		paddingTop:4,
 		paddingBottom:4,
 
-	}
+	},
+	quantityModal: {
+		width:widthQuanityModal,
+		height:heightQuanityModal,
+		position: 'absolute',
+		bottom:80,
+		right:30,
+		backgroundColor: '#e0e0e0',
+		// padding: 22,
+		// justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 4,
+		borderColor: 'rgba(0, 0, 0, 1)',
+		borderWidth:2
+	},
+	modalTotal: {
+		flex: .15,
+		flexDirection: 'row',
+		backgroundColor:'white',
+		alignItems:'center',
+		borderColor:'black',
+		borderWidth:4,
+		borderRadius:3
 
+}
 });
