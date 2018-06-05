9ce8fcdd7c3f05a7846db107f8922cc05854213b
Nick Rameau <r@meau.me>
2018-06-05 18:35:24 -0400

    Hotfixes

--- report_server/models/custom-methods.js
+++ report_server/models/custom-methods.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 module.exports = models => {
 	// Will execute everytime a user gets created or modified and the password has been changed
 	models.user.beforeSave(async (user, options) => {
-		if (user.changed('password')) return;
+		if (!user.changed('password')) return;
 
 		try {
 			user.password = await bcrypt.hashSync(user.password, process.env.BCRYPT_SALT_ROUNDS);
@@ -13,6 +13,6 @@ module.exports = models => {
 	});
 
 	// Instance level method: to use when comparing passwords on user login
-	models.user.prototype.comparePassword = () =>
+	models.user.prototype.comparePassword = (pw) =>
 		bcrypt.compareSync(pw, this.password);
 };
