3f9184612f6c7e62f1891f3496bf5b18b3d350fb
Nick Rameau <r@meau.me>
2018-06-06 16:39:42 -0400

    Add auth services

---
+++ report_server/seama_services/auth_services.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+
+// Simple function to take care of authentication
+const isAuthenticated = (req, res, next) =>
+	passport.authenticate('jwt', { session: false })(req, res, next);
+
+// Higher order function to take care of authorizations
+// It uses the 'code' column of the role table
+const isAuthorized = (...authorizedRoles) =>
+	(req, res, next) => {
+		const currentUserRoles = req.user.roles.map(roles => roles.code);
+
+		// Power to the admins
+		if (currentUserRoles.includes('admin')) return next();
+
+		const found = currentUserRoles.some(role =>
+			authorizedRoles.includes(role)
+		);
+
+		if (!found) {
+			// TODO: Improve this error message
+			return res.status(403).json('Forbidden');
+		}
+
+		next();
+	};
+
+module.exports = {
+	isAuthenticated,
+	isAuthorized
+};
