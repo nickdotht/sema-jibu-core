0e2a7f4b2c954efedbe4ce2a9dc912b078cdb7d1
Nick Rameau <r@meau.me>
2018-06-07 12:56:21 -0400

    Hotfix

--- report_server/bin/utils.js
+++ report_server/bin/utils.js
@@ -20,39 +20,39 @@ const normalizePort = val => {
 /**
  * Event listener for HTTP server "error" event.
  */
-const onError = error => {
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
-
-	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-	// handle specific listen errors with friendly messages
-	switch (error.code) {
-		case 'EACCES':
-			console.error(bind + ' requires elevated privileges');
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			console.error(bind + ' is already in use');
-			process.exit(1);
-			break;
-		default:
+const onError = port =>
+	error => {
+		if (error.syscall !== 'listen') {
 			throw error;
+		}
+
+		const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+		// handle specific listen errors with friendly messages
+		switch (error.code) {
+			case 'EACCES':
+				console.error(bind + ' requires elevated privileges');
+				process.exit(1);
+				break;
+			case 'EADDRINUSE':
+				console.error(bind + ' is already in use');
+				process.exit(1);
+				break;
+			default:
+				throw error;
+		}
 	}
-}
 
 /**
  * Event listener for HTTP server "listening" event.
  */
-const onListening = (server, debug) => {
-	return () => {
+const onListening = (server, debug) =>
+	() => {
 		const addr = server.address();
 		const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
 		debug('Listening on ' + bind);
 		console.log('Listening on ' + bind);
 	}
-}
 
 module.exports = {
 	normalizePort,
