93544cdfda20e50ce9507d86522269fdec26556c
Nick Rameau <r@meau.me>
2018-06-08 08:28:39 -0400

    Use our global config instead of the env vars

--- report_server/models/index.js
+++ report_server/models/index.js
@@ -8,23 +8,23 @@ const associations	= require('./associations');
 const customMethods	= require('./custom-methods');
 const db			= {};
 
-const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD, {
-	host: process.env.DB_HOST,
-	dialect: process.env.DB_DIALECT
+const sequelize = new Sequelize(__dbConfig.database, __dbConfig.username, __dbConfig.password, {
+	host: __dbConfig.host,
+	dialect: __dbConfig.dialect
 });
 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-	  return (file.indexOf('.') !== 0) &&
-		  (file !== basename) &&
-		  (!['associations.js', 'custom-methods.js'].includes(file)) &&
-		  (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-	const model = sequelize['import'](path.join(__dirname, file));
-	db[model.name] = model;
-  });
+	.readdirSync(__dirname)
+	.filter(file => {
+		return (file.indexOf('.') !== 0) &&
+		(file !== basename) &&
+		(!['associations.js', 'custom-methods.js'].includes(file)) &&
+		(file.slice(-3) === '.js');
+	})
+	.forEach(file => {
+		const model = sequelize['import'](path.join(__dirname, file));
+		db[model.name] = model;
+	});
 
 customMethods(db);
 associations(db);
