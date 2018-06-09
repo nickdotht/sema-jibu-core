db10fe06f12c7b41f94cae6eea1075b7f98da6b9
Nick Rameau <r@meau.me>
2018-06-09 10:34:37 -0400

    Use the db service as a global variable instead

--- report_server/seama_services/db_service.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-
-// Clone the global DB config and add a greater connection limit
-// (default is 5)
-const configs = {...__dbConfig, connectionLimit: 50};
-
-module.exports = {
-	connectionPool: mysql.createPool(configs);
-};
