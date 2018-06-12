123637873bd5b340fe61081ac101dc0754ee2ec4
Nick Rameau <r@meau.me>
2018-06-12 16:55:42 -0400

    Implement new login reducer

--- report_client/src/reducers/LoginReducer.js
+++ report_client/src/reducers/LoginReducer.js
@@ -1,17 +1,18 @@
 import {FETCH_LOGIN, RECEIVE_LOGIN, SET_LOGIN} from 'actions/ActionTypes';
 
-export default function login(state = {LogState:"NotLoggedIn"}, action) {
+export default (state = {logState: 'idle'}, action) => {
 	let newState;
+
 	switch (action.type) {
 		case FETCH_LOGIN:
 			console.log('FETCH_LOGIN Action');
 			return action;
 		case RECEIVE_LOGIN:
-			newState = action.LogState;
+			newState = action.logState;
 			console.log('RECEIVE_LOGIN Action');
 			return newState;
 		case SET_LOGIN:
-			newState = {LogState:action.LogState};
+			newState = {logState: action.logState};
 			console.log('SET_LOGIN Action');
 			return newState;
 		default:
