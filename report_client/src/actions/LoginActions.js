39e801b91610394aa7d3e5827047bb409bbff4d5
Nick Rameau <r@meau.me>
2018-06-12 16:52:58 -0400

    Implement new login actions and separate actions from services

--- report_client/src/actions/LoginActions.js
+++ report_client/src/actions/LoginActions.js
@@ -1,43 +1,33 @@
 import * as allActions from './ActionTypes';
+import { loginService } from '../services';
 
-export function receiveLogin(data) {
-	console.log("receiveLogin - ", data.toString())
-	return {type: allActions.RECEIVE_LOGIN, LogState: data};
+export const receiveLogin = data => {
+	console.log(`receiveLogin - ${JSON.stringify(data)}`);
+	return {type: allActions.RECEIVE_LOGIN, logState: data};
 }
 
-export function setLogin( logInState) {
-	console.log("setLogin")
-	return {type: allActions.SET_LOGIN, LogState: logInState};
-}
+export const fetchLogin = () => {
+	console.log('fetchLogin - loading');
+	return {type: allActions.FETCH_LOGIN, logState: 'loading'};
+};
 
-export function fetchLogin(usernameOrEmail, password) {
-	return (dispatch) => {
-		return fetch('/untapped/login', {
-			headers: {
-			  'Accept': 'application/json',
-			  'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ usernameOrEmail , password }),
-			method: 'post'
-		})
-		.then(response =>
-			response.json().then(data => ({
-				data:data,
-				status: response.status
-			}))
-		)
-		.then(response => {
-			if (response.status === 200) {
-				dispatch(receiveLogin(response.data))
-			} else {
-				const data = {LogState: "NoService" }
+export function login(usernameOrEmail, password) {
+	return dispatch => {
+		dispatch(fetchLogin());
+
+		loginService.login(usernameOrEmail, password)
+			.then(response => {
+				if (response.status === 200) {
+					dispatch(receiveLogin(response.data))
+				} else {
+					const data = { logState: "NoService" };
+					dispatch(receiveLogin(data))
+				}
+			})
+			.catch(function(error){
+				const data = { logState: "NoService" };
 				dispatch(receiveLogin(data))
-			}
-		})
-		.catch(function(error){
-			var data = {LogState: "NoService" }
-			dispatch(receiveLogin(data))
-		});
+			});
 	};
 }
 
