cfe9dfb97b7819e243440a3136dc71d7a1bb235d
Nick Rameau <r@meau.me>
2018-06-14 10:41:29 -0400

    Implement redux-persist

--- report_client/src/store/configureStore.js
+++ report_client/src/store/configureStore.js
@@ -1,11 +1,24 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from 'reducers/RootReducer';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+	key: 'root',
+	storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default function configureStore() {
-    return createStore(
-        rootReducer,
+    const store = createStore(
+        persistedReducer,
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
         applyMiddleware(thunk)
     );
+
+	const persistor = persistStore(store);
+
+	return { store, persistor };
 }
