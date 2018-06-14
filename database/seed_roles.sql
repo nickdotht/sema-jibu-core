c1b6659e303d338a85a41fcfc70d6e295cfd6970
fredoleary <fred@essentel.com>
2018-06-13 17:07:17 -0700

    added new files to seed db with credentials

---
+++ database/seed_roles.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM sema.role
+LIMIT 0, 200
+
+-- Date: 2018-06-12 11:53
+*/
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (3,'Admin','admin');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (4,'HQ Staff','hq');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (5,'Regional Manager','reg_manager');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (6,'Marketing','marketing');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (7,'Technical','technical');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (8,'Kiosk Supervisor','supervisor');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (9,'Kiosk Operator','operator');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (10,'Kiosk Assistant','assistant');
+INSERT INTO `role` (`id`,`authority`,`code`) VALUES (11,'Kiosk Driver','driver');
---
+++ database/seed_user_roles.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM sema.user_role
+LIMIT 0, 200
+
+-- Date: 2018-06-12 11:50
+*/
+INSERT INTO `user_role` (`role_id`,`user_id`) VALUES (3,1);
+INSERT INTO `user_role` (`role_id`,`user_id`) VALUES (3,2);
+INSERT INTO `user_role` (`role_id`,`user_id`) VALUES (3,3);
+INSERT INTO `user_role` (`role_id`,`user_id`) VALUES (4,1);
