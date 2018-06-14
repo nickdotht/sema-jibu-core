e812b1d4203c4f49bb32b41995b24179f6905c74
Nick Rameau <r@meau.me>
2018-06-14 10:26:28 -0400

    Code refactoring and implement auth states

--- report_client/src/components/SeamaToolbar.js
+++ report_client/src/components/SeamaToolbar.js
@@ -3,7 +3,7 @@ import { Navbar, Label, Nav,NavDropdown,MenuItem } from 'react-bootstrap';
 import 'App.css';
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
-import * as loginActions from 'actions/LoginActions';
+import { authActions } from 'actions';
 import * as healthCheckActions from 'actions/healthCheckActions';
 import * as kioskActions from 'actions/KioskActions';
 import * as waterOperationsActions from 'actions/WaterOperationsActions';
@@ -13,47 +13,47 @@ import { withRouter } from 'react-router'
 const menuStyle = {};
 
 const ImageStyle = {
-    resize:"both",
-    width:"180px",
-    height:"60px",
+	resize:"both",
+	width:"180px",
+	height:"60px",
 };
 
 const LabelStyleLeft = {
-    float:"left",
-    background:"none",
-    marginTop:"15px",
-    fontSize:"14px",
-    fontWeight: "normal"
+	float:"left",
+	background:"none",
+	marginTop:"15px",
+	fontSize:"14px",
+	fontWeight: "normal"
 };
 const LabelStyleRight = {
-    float:"right",
-    background:"none",
-    marginTop:"15px",
-    fontSize:"14px",
-    fontWeight: "normal"
+	float:"right",
+	background:"none",
+	marginTop:"15px",
+	fontSize:"14px",
+	fontWeight: "normal"
 };
 
 class SeamaToolbar extends Component {
-    constructor(props, context) {
-        super(props, context);
-        console.log("SeamaToolbar-constructor");
-        this.handleSelect = this.handleSelect.bind(this);
-        this.buildMenuItems = this.buildMenuItems.bind(this);
+	constructor(props, context) {
+		super(props, context);
+		console.log("SeamaToolbar-constructor");
+		this.handleSelect = this.handleSelect.bind(this);
+		this.buildMenuItems = this.buildMenuItems.bind(this);
 		this.logOut = this.logOut.bind(this);
 
-        this.state = {title: "--Kiosks--"};
-    }
+		this.state = {title: "--Kiosks--"};
+	}
 
-    componentDidMount() {
+	componentDidMount() {
 		this.props.healthCheckActions.fetchHealthCheck();
 		this.props.kioskActions.fetchKiosks();
-    }
+	}
 
-    handleSelect(eventKey){
-        console.log(eventKey, this.props.kiosk.kiosks[eventKey].name);
-        this.setState({title: this.props.kiosk.kiosks[eventKey].name});
-        let kioskParams = {kioskID:this.props.kiosk.kiosks[eventKey].id};
-        if( kioskParams.kioskID !== this.props.kiosk.selectedKiosk.kioskID) {
+	handleSelect(eventKey){
+		console.log(eventKey, this.props.kiosk.kiosks[eventKey].name);
+		this.setState({title: this.props.kiosk.kiosks[eventKey].name});
+		let kioskParams = {kioskID:this.props.kiosk.kiosks[eventKey].id};
+		if( kioskParams.kioskID !== this.props.kiosk.selectedKiosk.kioskID) {
 			kioskParams.groupby = "month";	// TODO - Should be derived from toolbar time/date filter UI
 			this.props.kioskActions.selectKiosk(kioskParams);
 			this.props.waterOperations.loaded = false;
@@ -70,55 +70,58 @@ class SeamaToolbar extends Component {
 			}
 
 		}
-    };
+	};
 
-    buildMenuItems(){
-        let menuItems = [];
-        if( this.props.kiosk.kiosks){
-            let keys = Object.keys(this.props.kiosk.kiosks);
-            for( let i = 0; i < keys.length; i++ ){
-                let kiosk = this.props.kiosk.kiosks[keys[i]];
-                 menuItems.push(<MenuItem eventKey={keys[i]} style={menuStyle}>{kiosk.name}</MenuItem>);
-            }
-        }
-        return menuItems;
-    }
-    logOut (){
-        console.log("logOut");
-		this.props.loginActions.logOut();
-    }
+	buildMenuItems(){
+		let menuItems = [];
+		if( this.props.kiosk.kiosks){
+			let keys = Object.keys(this.props.kiosk.kiosks);
+			for( let i = 0; i < keys.length; i++ ){
+				let kiosk = this.props.kiosk.kiosks[keys[i]];
+				 menuItems.push(<MenuItem eventKey={keys[i]} style={menuStyle}>{kiosk.name}</MenuItem>);
+			}
+		}
+		return menuItems;
+	}
+	logOut (){
+		console.log("logout");
+		this.props.authActions.logout();
+		window.location.reload(true);
+	}
 
-    render() {
-        return (
-            <div className="SeamaNavToolbar" >
-                <Navbar bsStyle="inverse" style={{marginBottom:"0px"}}>
-                    <Navbar.Header >
-                        <Navbar.Brand>
-                            {<img src={require('images/dlo_image.png')} alt="logo" style={ImageStyle} />}
-                        </Navbar.Brand>
-                    </Navbar.Header>
-                    <Label style={LabelStyleLeft}>
-                        Version {this.props.Version}
-                    </Label>
-                    <Label style={LabelStyleLeft}>
-                        Kiosk
-                    </Label>
-                    <Nav >
-                        <NavDropdown eventKey={3} title={this.state.title} onSelect={this.handleSelect} id="basic-nav-dropdown" >
-                            {this.buildMenuItems()}
-                        </NavDropdown>
-                    </Nav>
+	render() {
+		return (
+			<div className="SeamaNavToolbar" >
+				<Navbar bsStyle="inverse" style={{marginBottom:"0px",
+					borderRadius: 0}}>
+					<Navbar.Header >
+						<Navbar.Brand>
+							{<img src={require('images/dlo_image.png')} alt="logo" style={ImageStyle} />}
+						</Navbar.Brand>
+					</Navbar.Header>
+					<Label style={LabelStyleLeft}>
+						Version {this.props.Version}
+					</Label>
+					<Label style={LabelStyleLeft}>
+						Kiosk
+					</Label>
+					<Nav >
+						<NavDropdown eventKey={3} title={this.state.title} onSelect={this.handleSelect} id="basic-nav-dropdown" >
+							{this.buildMenuItems()}
+						</NavDropdown>
+					</Nav>
 					<Label style={LabelStyleRight}> Server: {this.showServer()}</Label>
-                    <Label  eventKey={1} onClick={this.logOut} href="#" style={LabelStyleRight}>
-                        Logout
-                    </Label>
-                    <Label style={LabelStyleRight}>
-                        Fred O'Leary
-                    </Label>
-                </Navbar>
-            </div>
-        );
-    }
+					<Label	eventKey={1} onClick={this.logOut} href="#" style={LabelStyleRight}>
+						<a href="/">Logout</a>
+					</Label>
+					<Label style={LabelStyleRight}>
+						{ `${this.props.currentUser.first_name}
+						${this.props.currentUser.last_name}`}
+					</Label>
+				</Navbar>
+			</div>
+		);
+	}
 	showServer () {
 		if (this.props.healthCheck.server === "Ok") {
 			return this.props.healthCheck.version;
@@ -129,7 +132,8 @@ class SeamaToolbar extends Component {
 
 function mapStateToProps(state) {
 	return {
-		logState: state.logIn.LogState,
+		logState: state.auth.LogState,
+		currentUser: state.auth.currentUser,
 		healthCheck: state.healthCheck,
 		kiosk:state.kiosk,
 		waterOperations:state.waterOperations,
@@ -139,7 +143,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
 	return {
-		loginActions: bindActionCreators(loginActions, dispatch),
+		authActions: bindActionCreators(authActions, dispatch),
 		healthCheckActions: bindActionCreators(healthCheckActions, dispatch),
 		kioskActions: bindActionCreators(kioskActions, dispatch),
 		waterOperationsActions: bindActionCreators(waterOperationsActions, dispatch),
