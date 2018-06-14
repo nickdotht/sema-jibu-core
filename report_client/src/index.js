3b6d0e7b853bc275f092707605e5c0e25cd92fef
Nick Rameau <r@meau.me>
2018-06-14 10:35:44 -0400

    Implement redux-persist

--- report_client/src/index.js
+++ report_client/src/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from 'store/configureStore';
-
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 //import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'bootstrap/dist/css/bootstrap_cerulean.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
-    <Provider  store={ store }>
-		<App />
+    <Provider store={ store }>
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
     </Provider>,
 	document.getElementById('root')
 );
