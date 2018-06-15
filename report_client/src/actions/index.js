bd8af01e802732889aa2465e211b96891a92159f
Nick Rameau <r@meau.me>
2018-06-15 16:12:53 -0400

    Connect more actions

--- report_client/src/actions/index.js
+++ report_client/src/actions/index.js
@@ -1,2 +1,6 @@
 export * from './AuthActions';
 export * from './ActionTypes';
+export * from './healthCheckActions';
+export * from './KioskActions';
+export * from './SalesActions';
+export * from './WaterOperationsActions';
