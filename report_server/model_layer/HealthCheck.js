b9667d854639af854324ce29ace002c8a3ed72cd
fredoleary <fred@essentel.com>
2018-06-18 12:39:42 -0700

    DLO1-77 - Initial model layout implementation

---
+++ report_server/model_layer/HealthCheck.js
@@ -0,0 +1,23 @@
+/// HealthCheck class - Validates that web service is functional
+class HealthCheck {
+	constructor(request, schema) {
+		this.server = "Ok";
+		this.database = "Ok";
+		this.version = request.app.get('sema_version');
+		this.schema = schema;
+	}
+	set server( status){
+		this._server = status;
+	}
+	set database( status){
+		this._database = status;
+	}
+	classToPlain(){
+		return {server:this._server,
+			database:this._database,
+			version:this._version,
+			schema:this._schema};
+	}
+}
+
+module.exports = HealthCheck;
--- report_server/routes/sema_health_check.js
+++ report_server/routes/sema_health_check.js
@@ -2,31 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const semaLog = require('../seama_services/sema_logger');
+const HealthCheck = require('../model_layer/HealthCheck');
 
 router.get('/', (req, res) => {
 	semaLog.log('info', 'health-check - Enter');
 
 	__pool.getConnection((err, connection) => {
 		connection.release();
-
+		let healthCheck = new HealthCheck( req, connection.config.database);
 		if (err) {
 			semaLog.log('error', 'health-check - Failed; database failure');
 			semaLog.log('error', err.toString());
-
-			return res.json({
-				server: 'Ok',
-				database: 'Failed',
-				version: req.app.get('sema_version')
-			});
+			healthCheck.database = "Failed";
+		}else{
+			semaLog.log('info', 'health-check - succeeded');
 		}
-
-		semaLog.log('info', 'health-check - succeeded');
-
-		res.json({
-			server: 'Ok',
-			database: 'Ok',
-			version: req.app.get('sema_version')
-		});
+		res.json(healthCheck.classToPlain());
 	});
 });
 
