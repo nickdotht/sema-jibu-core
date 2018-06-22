2fa81914256bdaa45ec7d9214fcb62865f7d019c
Nick Rameau <r@meau.me>
2018-06-22 16:21:12 -0400

    Initial promise handlers

--- report_server/bin/global_vars.js
+++ report_server/bin/global_vars.js
@@ -1,13 +1,30 @@
-const path = require('path');
-const mysql = require('mysql2');
+/*
+ * Set global variables here. Make sure they start
+ * with "__"
+ */
+
+const path	= require('path');
+const mysql	= require('mysql2');
+const pe	= require('parse-error');
 const env	= process.env.NODE_ENV || 'development';
 
-// Set global variables here. Make sure they start
-// with "__"
 __basedir	= path.resolve(__dirname, '..');
 __dbConfig	= require(`${__basedir}/config/database`)[env];
 
 // Clone the global DB config and add additional options
-// (default is 5)
 const configs = {...__dbConfig, connectionLimit: 50, supportBigNumbers: true, decimalNumbers: true};
+
 __pool = mysql.createPool(configs);
+
+__to = promise =>
+		promise
+		.then(data => [null, data])
+		.catch(err => [pe(err)]);
+
+__te = (err_message, log) => { // te stands for Throw Error
+	if(log === true){
+		console.error(err_message);
+	}
+
+	throw new Error(err_message);
+}
