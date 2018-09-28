164a880ddbe60755639437ef84b8511682840fa5
fredoleary <fred@essentel.com>
2018-09-28 16:26:14 -0700

    Continue dloHaiti import

--- database/python_scripts/utilities/DBReadHaiti.py
+++ database/python_scripts/utilities/DBReadHaiti.py
@@ -17,86 +17,73 @@ class DBReadHaiti:
 
     """ Get all countries  """
     def read_countries( self):
-        countries = []
+        countries = {}
         cursor = self.connection.cursor()
-        cursor.execute("SELECT name FROM country")
+        cursor.execute("SELECT id, name FROM country")
         rows = cursor.fetchall()
         for row in rows:
-            countries.append( row[0])
-
+            countries[row[0]] = row[1]
         cursor.close()
         return countries
 
     """ Get all regions  """
-    def read_regions( self):
-        regions = []
+    def read_regions( self, countries):
+        regions = {}
         cursor = self.connection.cursor()
-        cursor.execute("SELECT country_id, name FROM region")
+        cursor.execute("SELECT id, name, country_id FROM region")
         rows = cursor.fetchall()
         for row in rows:
-            cursor2 = self.connection.cursor()
-            cursor2.execute("SELECT name FROM country where id = %s", (row[0],))
-            countries = cursor2.fetchall()
-            regions.append( [countries[0][0], row[1]])
-            cursor2.close()
+            regions[row[0]] = {"name":row[1], "country":countries[row[2]]}
 
         cursor.close()
         return regions
 
 
     """ Get all kiosks  """
-    def read_kiosks(self):
-        kiosks = []
+    def read_kiosks(self, regions):
+        kiosks = {}
         cursor = self.connection.cursor()
-        cursor.execute("SELECT region_id, name FROM kiosk")
+        cursor.execute("SELECT id, name, region_id FROM kiosk")
         rows = cursor.fetchall()
         for row in rows:
-            cursor2 = self.connection.cursor()
-            cursor2.execute("SELECT name FROM region where id = %s", (row[0],))
-            regions = cursor2.fetchall()
-            kiosks.append([regions[0][0], row[1]])
-            cursor2.close()
-
+            kiosks[row[0]] = {"name": row[1], "region": regions[row[2]]}
         cursor.close()
         return kiosks
 
     """ Get all customerTypes  """
     def read_customer_types( self):
-        customer_types = []
+        customer_types = {}
         cursor = self.connection.cursor()
-        cursor.execute("SELECT name FROM customer_type")
+        cursor.execute("SELECT id, name FROM customer_type")
         rows = cursor.fetchall()
         for row in rows:
-            customer_types.append( row[0])
-
+            customer_types[row[0]] = row[1]
         cursor.close()
         return customer_types
 
     """ Get all sales_channels  """
     def read_sales_channels(self):
-        sales_channels = []
+        sales_channels = {}
         cursor = self.connection.cursor()
-        cursor.execute("SELECT name FROM sales_channel")
+        cursor.execute("SELECT id, name FROM sales_channel")
         rows = cursor.fetchall()
         for row in rows:
-            sales_channels.append( row[0])
+             sales_channels[row[0]] = row[1]
 
         cursor.close()
         return sales_channels
 
     """ Get all customers  """
     def read_customers(self):
-        customers = []
+        customers = {}
         cursor = self.connection.cursor()
-        cursor.execute("SELECT id, address, contact_name, customer_type_id, due_amount, gps_coordinates, kiosk_id, "
-                       "servicable_customer_base phone_number FROM customer_account")
+        cursor.execute("SELECT id, address, contact_name, customer_type_id, due_amount, gps_coordinates, kiosk_id," 
+                       "phone_number, active, serviceable_customer_base FROM customer_account")
         rows = cursor.fetchall()
         for row in rows:
-            cursor2 = self.connection.cursor()
-            cursor2.execute("SELECT name FROM region where id = %s", (row[0],))
-            regions = cursor2.fetchall()
-            customers.append([regions[0][0], row[1]])
-            cursor2.close()
+            customers[row[0]] = {"id":row[0], "address": row[1], "contact_name": row[2], "customer_type_id":row[3],
+                                 "due_amount":row[4], "gps_coordinates":row[5], "kiosk_id":row[6], "phone_number":row[7],
+                                 "active":row[8], "servicable_customer_base":row[9]}
 
         cursor.close()
         return customers
