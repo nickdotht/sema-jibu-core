d271c5de9e9a15aaba8dd022d04aaadfe87dc2ec
fredoleary <fred@essentel.com>
2018-10-19 10:01:57 -0700

    Updated views to include additional columns

--- database/customer_details_view.sql
+++ database/customer_details_view.sql
@@ -6,9 +6,10 @@ CREATE VIEW `customer_details` AS
         customer_account.income_level,
         customer_account.gender,
         customer_account.distance,
+        customer_account.consumer_base as customer_consumer_base,
         kiosk.name as kiosk_name,
         kiosk.id as kiosk_id,
-		kiosk.consumer_base
+		kiosk.consumer_base as kiosk_consumer_base
    FROM
         customer_account
             INNER JOIN
