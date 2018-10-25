d21127c409204ec7263e98aa9ecea402f19c2323
mike1e <mike.le07@gmail.com>
2018-10-25 01:02:57 -0700

    [Refactor] Refactor Create and Update User to use async/await

--- report_server/routes/sema_user.js
+++ report_server/routes/sema_user.js
@@ -37,7 +37,7 @@ router.get('/', function(req, res) {
 		.catch(err => res.status(500).json({ error: err }));
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 	semaLog.info('create user - enter');
 	const {
 		username,
@@ -45,140 +45,83 @@ router.post('/', (req, res) => {
 		password,
 		firstName,
 		lastName,
-		roles
+		role
 	} = req.body.data;
 	let assignedRoles;
-	let createdUser;
-
-	db.user
-		.findOne({
-			where: {
-				[Op.or]: [{ email: email }, { username: username }]
-			}
-		})
-		.then(user => {
-			if (user) {
-				semaLog.info('Email/username already exists');
-				res.status(400).json({
-					error: 'Email/username already exists'
-				});
-			} else {
-				db.role
-					.findAll({
-						where: {
-							code: [roles]
-						}
-					})
-					.then(dbRoles => {
-						if (!dbRoles) {
-							res.status(400).json({
-								error: 'Role(s) do not exist'
-							});
-						}
-						assignedRoles = dbRoles;
-						return db.user.create({
-							username,
-							email,
-							password,
-							first_name: firstName,
-							last_name: lastName
-						});
-					})
-					.then(newUser => {
-						semaLog.info('User was created');
-						createdUser = newUser;
-						return assignedRoles.forEach(role =>
-							role.addUser(createdUser)
-						);
-					})
-					.then(result => {
-						semaLog.info('add role(s) to user');
-						res.json({
-							message: 'create user success',
-							user: createdUser.toJSON()
-						});
-					})
-					.catch(err => {
-						semaLog.error(`Create user with role - failed ${err}`);
-						res.status(400).json({
-							message: 'ERROR',
-							err
-						});
-					});
-			}
-		})
-		.catch(err => {
-			semaLog.error(`Create user failed - ${err}`);
-			res.status(400).json({ error: 'Failed to create user' });
+	try {
+		const user = await db.user.findOne({
+			where: { [Op.or]: [{ email: email }, { username: username }] }
 		});
+		if (user) {
+			semaLog.info('Email/username already exists');
+			throw new Error('User already exists');
+		}
+
+		const createdUser = await db.user.create({
+			username,
+			email,
+			password,
+			first_name: firstName,
+			last_name: lastName
+		});
+		semaLog.info('User created success');
+
+		const dbRoles = await db.role.findAll({ where: { code: [role] } });
+		dbRoles.forEach(role => role.addUser(createdUser));
+		semaLog.info('Roles added to user success');
+
+		res.json({
+			message: 'create user success',
+			user: createdUser.toJSON()
+		});
+	} catch (err) {
+		semaLog.error(`Create user failed - ${err}`);
+		res.status(400).json({
+			message: `Failed to create user ${err}`,
+			error: `${err}`
+		});
+	}
 });
 
-router.put('/toggle/:id', (req, res) => {
-	semaLog.info('Users activate/deactivate - Enter ');
-	db.user
-		.find({
-			where: {
-				id: req.params.id
-			}
-		})
-		.then(user => {
-			if (!user) {
-				res.status(404).json({
-					message: 'User not found'
-				});
-			}
-			user.update({
-				active: !user.active
-			}).then(updatedUser => {
-				res.status(200).json({
-					message: `User ${
-						user.active ? 'is active' : 'is deactive'
-					}`,
-					user: user.toJSON()
-				});
-			});
-		})
-		.catch(err => {
-			res.status(400).json({
-				messsage: `User toggle update failed`
-			});
+router.put('/:id', async (req, res) => {
+	semaLog.info(`UPDATE user ${req.params.id} - enter`);
+	const {
+		username,
+		email,
+		password,
+		firstName,
+		lastName,
+		role
+	} = req.body.data;
+
+	try {
+		const user = await db.user.find({ where: { id: req.params.id } });
+		if (!user) {
+			throw new Error('User not found');
+		}
+		let updatedUser = await user.update({
+			username,
+			email,
+			password,
+			first_name: firstName,
+			last_name: lastName
 		});
-});
-router.put('/:id', (req, res) => {
-	const { username, email, password, firstName, lastName } = req.body.data;
-	db.user
-		.find({
-			where: {
-				id: req.params.id
-			}
-		})
-		.then(user => {
-			if (!user) {
-				res.status(404).json({
-					message: 'User not found'
-				});
-			}
-			user.update({
-				username,
-				email,
-				password,
-				first_name: firstName,
-				last_name: lastName
-			})
-				.then(updatedUser =>
-					res
-						.status(200)
-						.json({ message: 'User updated', user: user.toJSON() })
-				)
-				.catch(err => {
-					semaLog.error(`Update user failed - ${err}`);
-					res.status(400).json({ error: err });
-				});
-		})
-		.catch(err => {
-			semaLog.error(`Update user - find user fail - ${err}`);
-			res.status(400).json({ error: err });
+
+		await updatedUser.setRoles([]); // clear roles
+		const dbRoles = await db.role.findAll({ where: { code: [role] } });
+		dbRoles.forEach(role => role.addUser(updatedUser));
+
+		res.json({
+			message: 'Update user success',
+			user: Object.assign({ ...updatedUser.toJSON() }, { role })
 		});
+	} catch (err) {
+		semaLog.error(`Update user failed - ${err}`);
+		res.status(400).json({
+			message: `Update user failed - ${err}`,
+			error: `${err}`
+		});
+	}
 });
 
 router.delete('/:id', (req, res) => {
@@ -216,6 +159,38 @@ router.delete('/:id', (req, res) => {
 		.catch(err => {
 			semaLog.error('Delete user - user not found');
 			res.status(400).json({ error: err });
+		});
+});
+
+router.put('/toggle/:id', (req, res) => {
+	semaLog.info('Users activate/deactivate - Enter ');
+	db.user
+		.find({
+			where: {
+				id: req.params.id
+			}
+		})
+		.then(user => {
+			if (!user) {
+				res.status(404).json({
+					message: 'User not found'
+				});
+			}
+			user.update({
+				active: !user.active
+			}).then(updatedUser => {
+				res.status(200).json({
+					message: `User ${
+						user.active ? 'is active' : 'is deactive'
+					}`,
+					user: user.toJSON()
+				});
+			});
+		})
+		.catch(err => {
+			res.status(400).json({
+				messsage: `User toggle update failed`
+			});
 		});
 });
 
