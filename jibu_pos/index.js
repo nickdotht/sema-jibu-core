64e7efea080db6807718305a6ebf2be87ac62a2c
Nick Rameau <r@meau.me>
2018-10-30 20:15:18 -0400

    App rename

--- jibu_pos/index.js
+++ jibu_pos/index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/app/App';
 
-AppRegistry.registerComponent('jibu_pos', () => App);
+AppRegistry.registerComponent('SEMAPOS', () => App);
