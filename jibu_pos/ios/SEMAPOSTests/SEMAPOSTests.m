ac6608976f752b1ca3655a39aef4ff347457f112
Nick Rameau <r@meau.me>
2018-10-30 20:10:42 -0400

    iOS debranding

--- jibu_pos/ios/SEMAPOSTests/SEMAPOSTests.m
+++ jibu_pos/ios/SEMAPOSTests/SEMAPOSTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface jibu_posTests : XCTestCase
+@interface SEMAPOSTests : XCTestCase
 
 @end
 
-@implementation jibu_posTests
+@implementation SEMAPOSTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
