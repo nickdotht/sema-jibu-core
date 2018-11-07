7d1be0e421aea999fe6327858da8da0e2e5f4f69
Nick Rameau <r@meau.me>
2018-11-07 12:39:14 -0500

    [Refactor] Get rid of unnecessary code

--- mobile_client/src/components/orders/OrderProductScreen.js
+++ mobile_client/src/components/orders/OrderProductScreen.js
@@ -1,58 +1,12 @@
-import React, {Component}  from "react";
-import { View, Text, FlatList, TouchableHighlight, StyleSheet } from "react-native";
-import ProductList from "./ProductList";
 import {createBottomTabNavigator} from "react-navigation";
-import {bindActionCreators} from "redux";
-import * as OrderActions from "../../actions/OrderActions";
-import {connect} from "react-redux";
-import OrderResellerScreen from './OrderResellerScreen';
-import OrderWalkupScreen from './OrderWalkupScreen';
+import ProductListScreen from './ProductListScreen';
 
-
-
-
-
-export const OrderProductScreen = createBottomTabNavigator({
-	Walkup: {
-		screen: OrderWalkupScreen,
+export default OrderProductScreen = createBottomTabNavigator({
+	ProductList: {
+		screen: ProductListScreen,
 		navigationOptions: {
-			tabBarLabel: 'Walkup',
-			tabBarVisible: false,
-			tabBarOnPress: (scene, jumpToIndex) => {
-				let output = "Walkup-Tab " + scene.navigation.state.routeName;
-				console.log(output);
-			},
-		},
-
-	},
-
-	Reseller: {
-		screen: OrderResellerScreen,
-		navigationOptions: {
-			tabBarLabel: 'Reseller',
-			tabBarVisible: false,
-			tabBarOnPress: (scene, jumpToIndex) => {
-				let output = "Reseller-Tab " + scene.navigation.state.routeName;
-				console.log(output);
-			},
-		},
-
-	},
-
-},{
-	swipeEnabled: true,
-	tabBarOptions: {
-		activeTintColor: '#F0F0F0',
-		activeBackgroundColor: "#18376A",
-		inactiveTintColor: '#000000',
-		inactiveBackgroundColor: 'white',
-		style: { borderTopColor:'black', borderTopWidth:3},
-		// style: {padding:0, margin:0, borderColor:'red', borderWidth:5, justifyContent: 'center', alignItems: 'center' },
-		labelStyle: {
-			fontSize: 18,
-			padding: 12
-		},
-		tabStyle: {justifyContent: 'center', alignItems: 'center'},
+			tabBarVisible: false
+		}
 	}
 });
 
