628216da81dcb8680177c47f34f82354a31170b9
mike1e <mike.le07@gmail.com>
2018-11-27 23:14:07 -0800

    Revert "[Patch] Fix product test to include 6 mrps"
    This reverts commit 09f2f52100c52c5abbc6c5d67f4accb12668d7af.

--- report_server/test/testProducts.js
+++ report_server/test/testProducts.js
@@ -10,67 +10,53 @@ let findKioskId = require('./Utilities/findKioskId');
 let findCustomerId = require('./Utilities/findCustomerId');
 let findProductId = require('./Utilities/findProductId');
 
-describe('Testing Products API', function() {
+
+describe('Testing Products API', function () {
 	let server;
 	this.timeout(6000);
-	beforeEach(done => {
+	beforeEach( (done) => {
 		var iAmDone = done;
-		server = require('../bin/www');
-		setTimeout(function() {
-			iAmDone();
-		}, 1500);
+		server = require('../bin/www' );
+		setTimeout( function(){iAmDone()}, 1500);
 	});
-	afterEach(done => {
+	afterEach( (done) => {
 		var iAmDone = done;
 		server.close();
-		setTimeout(function() {
-			iAmDone();
-		}, 2000);
+		setTimeout( function(){iAmDone()}, 2000);
 	});
 
 	describe('GET /sema/products - updated-date 2016-1-1', function() {
-		it('Should return info on 2 products after 2016-1-1', done => {
+		it('Should return info on 2 products after 2016-1-1', (done) => {
 			authenticate(server).then(function(token) {
 				chai.request(server)
-					.get('/sema/products?updated-date=2016-1-1')
+					.get("/sema/products?updated-date=2016-1-1")
 					.set('Authorization', token)
 					.end(function(err, res) {
 						res.should.have.status(200);
 						expect(res.body.products).to.be.an('array');
 						expect(res.body.products.length).to.be.equal(2);
-						expect(res.body.products[0].description).to.be.equal(
-							'Description Product-1'
-						);
-						expect(res.body.products[0].updatedDate).to.be.equal(
-							'2018-01-01T08:00:00.000Z'
-						);
-						expect(res.body.products[0].priceCurrency).to.be.equal(
-							'USD'
-						);
-						expect(res.body.products[0].priceAmount).to.be.equal(
-							5.0
-						);
-						expect(res.body.products[0].unitPerProduct).to.be.equal(
-							1
-						);
-						expect(res.body.products[0].unitMeasure).to.be.equal(
-							'tons'
-						);
-						expect(res.body.products[0].cogsAmount).to.be.equal(
-							4.0
-						);
+						expect(res.body.products[0].description).to.be.equal("Description Product-1");
+						expect(res.body.products[0].updatedDate).to.be.equal("2018-01-01T08:00:00.000Z");
+						expect(res.body.products[0].priceCurrency).to.be.equal("USD");
+						expect(res.body.products[0].priceAmount).to.be.equal(5.00);
+						expect(res.body.products[0].unitPerProduct).to.be.equal(1);
+						expect(res.body.products[0].unitMeasure).to.be.equal("tons");
+						expect(res.body.products[0].cogsAmount).to.be.equal(4.00);
+
 
 						done(err);
 					});
+
+
 			});
 		});
 	});
 
 	describe('GET /sema/site/products - no update-date', function() {
-		it('Should return info on all 4 products', done => {
+		it('Should return info on all 4 products', (done) => {
 			authenticate(server).then(function(token) {
 				chai.request(server)
-					.get('/sema/products')
+					.get("/sema/products")
 					.set('Authorization', token)
 					.end(function(err, res) {
 						res.should.have.status(200);
@@ -83,105 +69,57 @@ describe('Testing Products API', function() {
 	});
 
 	describe('GET Should return product mrp info on product/kiosk/salesChannel', function() {
-		it('Should pass, returning all 5 product mrps', done => {
+		it('Should pass, returning all 5 product mrps', (done) => {
 			authenticate(server).then(function(token) {
-				findKioskId
-					.findKioskId(server, token, 'UnitTestCustomers')
-					.then(function(kiosk) {
-						findProductId
-							.findProductId(server, token, 'sku1')
-							.then(function(product) {
-								let url = sprintf(
-									'/sema/site/product-mrps?site-id=%d',
-									kiosk.id
-								);
-								chai.request(server)
-									.get(url)
-									.set('Authorization', token)
-									.end(function(err, res) {
-										expect(res.body.productMRPs).to.be.an(
-											'array'
-										);
-										expect(
-											res.body.productMRPs.length
-										).to.be.equal(6);
-										res.should.have.status(200);
-										for (
-											let index = 0;
-											index < res.body.productMRPs.length;
-											index++
-										) {
-											if (
-												res.body.productMRPs[index]
-													.productId == product.id
-											) {
-												expect(
-													res.body.productMRPs[index]
-														.priceAmount
-												).to.be.equal(10);
-												expect(
-													res.body.productMRPs[index]
-														.cogsAmount
-												).to.be.equal(4);
-											}
-										}
-										done(err);
-									});
-							});
+				findKioskId.findKioskId(server, token, 'UnitTestCustomers').then(function(kiosk) {
+					findProductId.findProductId(server, token, 'sku1').then(function(product) {
+						let url = sprintf("/sema/site/product-mrps?site-id=%d", kiosk.id);
+						chai.request(server)
+							.get(url)
+							.set('Authorization', token)
+							.end(function(err, res) {
+								expect(res.body.productMRPs).to.be.an('array');
+								expect(res.body.productMRPs.length).to.be.equal(5);
+								res.should.have.status(200);
+								for( let index = 0; index < res.body.productMRPs.length; index++ ){
+									if( res.body.productMRPs[index].productId == product.id ){
+										expect(res.body.productMRPs[index].priceAmount).to.be.equal(10);
+										expect(res.body.productMRPs[index].cogsAmount).to.be.equal(4);
+									}
+								}
+								done(err);
+						});
 					});
+				});
 			});
 		});
 	});
 
 	describe('GET Should return product mrp info on product/kiosk/salesChannel AFTER 1/1/2017', function() {
-		it('Should pass, returning all 1 product mrps', done => {
+		it('Should pass, returning all 1 product mrps', (done) => {
 			authenticate(server).then(function(token) {
-				findKioskId
-					.findKioskId(server, token, 'UnitTestCustomers')
-					.then(function(kiosk) {
-						findProductId
-							.findProductId(server, token, 'sku1')
-							.then(function(product) {
-								let url = sprintf(
-									'/sema/site/product-mrps?site-id=%d&updated-date=%s',
-									kiosk.id,
-									new Date('2017-1-1')
-								);
-								chai.request(server)
-									.get(url)
-									.set('Authorization', token)
-									.end(function(err, res) {
-										expect(res.body.productMRPs).to.be.an(
-											'array'
-										);
-										expect(
-											res.body.productMRPs.length
-										).to.be.equal(1);
-										res.should.have.status(200);
-										for (
-											let index = 0;
-											index < res.body.productMRPs.length;
-											index++
-										) {
-											if (
-												res.body.productMRPs[index]
-													.productId == product.id
-											) {
-												expect(
-													res.body.productMRPs[index]
-														.priceAmount
-												).to.be.equal(10);
-												expect(
-													res.body.productMRPs[index]
-														.cogsAmount
-												).to.be.equal(4);
-											}
-										}
-										done(err);
-									});
+				findKioskId.findKioskId(server, token, 'UnitTestCustomers').then(function(kiosk) {
+					findProductId.findProductId(server, token, 'sku1').then(function(product) {
+						let url = sprintf("/sema/site/product-mrps?site-id=%d&updated-date=%s", kiosk.id, new Date("2017-1-1"));
+						chai.request(server)
+							.get(url)
+							.set('Authorization', token)
+							.end(function(err, res) {
+								expect(res.body.productMRPs).to.be.an('array');
+								expect(res.body.productMRPs.length).to.be.equal(1);
+								res.should.have.status(200);
+								for( let index = 0; index < res.body.productMRPs.length; index++ ){
+									if( res.body.productMRPs[index].productId == product.id ){
+										expect(res.body.productMRPs[index].priceAmount).to.be.equal(10);
+										expect(res.body.productMRPs[index].cogsAmount).to.be.equal(4);
+									}
+								}
+								done(err);
 							});
 					});
+				});
 			});
 		});
 	});
+
 });
