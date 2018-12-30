69b0ba04f9e47f73b5b658be59506a6c12fdd05e
Nick Rameau <r@meau.me>
2018-12-30 15:05:37 -0500

    Display table with the right headers

--- mobile_client/src/components/reports/SalesLogging.js
+++ mobile_client/src/components/reports/SalesLogging.js
@@ -3,12 +3,17 @@ import { Text, View, StyleSheet } from 'react-native';
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as reportActions from "../../actions/ReportActions";
+import { Table, Row } from 'react-native-table-component';
 
 import i18n from '../../app/i18n';
 
 class SalesLogging extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            tableHead: ['Date', 'Name', 'SKU', 'Quantity', 'Cost', 'Action']
+        }
     }
 
     componentDidMount() {
@@ -18,8 +23,10 @@ class SalesLogging extends Component {
     render() {
         if (this.props.reportType === "salesLogging") {
             return (
-                <View style={{ flex: 1 }}>
-                    <Text>Hello</Text>
+                <View style={styles.table_container}>
+                    <Table borderStyle={{ borderWidth: 0 }}>
+                        <Row data={this.state.tableHead} style={styles.table_header} textStyle={styles.table_header_text} />
+                    </Table>
                 </View>
             );
         }
@@ -42,4 +49,18 @@ function mapDispatchToProps(dispatch) {
 export default connect(mapStateToProps, mapDispatchToProps)(SalesLogging);
 
 const styles = StyleSheet.create({
+    table_container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+
+    table_header: {
+        height: 40,
+        backgroundColor: '#ABC1DE'
+    },
+
+    table_header_text: {
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    },
 });
