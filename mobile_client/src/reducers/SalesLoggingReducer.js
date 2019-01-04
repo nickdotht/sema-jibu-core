932d49bd715eca689b513405a6c2e439b13259e1
Nick Rameau <r@meau.me>
2019-01-03 23:56:31 -0500

    [Fix] Return the right state data

--- mobile_client/src/reducers/SalesLoggingReducer.js
+++ mobile_client/src/reducers/SalesLoggingReducer.js
@@ -7,12 +7,9 @@ let initialState = {
 
 const salesLoggingReducer = (state = initialState, action) => {
     console.log("salesLoggingReducer: " + action.type);
-    let newState;
     switch (action.type) {
         case SET_REMOTE_SALES:
-            newState = { ...state };
-            newState.products = newState.products.concat(action.data);
-            return newState;
+            return action.data;
         default:
             return state;
     }
