188ca8e29fa1acb480a361b35fd3dafc6581f394
Nick Rameau <r@meau.me>
2019-01-07 12:59:38 -0500

    [Patch] Rename actions file

---
+++ mobile_client/src/actions/ReceiptActions.js
@@ -0,0 +1,79 @@
+import PosStorage from "../database/PosStorage";
+
+export const SET_REMOTE_RECEIPTS = 'SET_REMOTE_RECEIPTS';
+export const ADD_LOCAL_RECEIPT = 'ADD_LOCAL_RECEIPT';
+export const SET_LOCAL_RECEIPTS = 'SET_LOCAL_RECEIPTS';
+export const UPDATE_REMOTE_RECEIPT = 'UPDATE_REMOTE_RECEIPT';
+export const UPDATE_LOCAL_RECEIPT = 'UPDATE_LOCAL_RECEIPT';
+export const UPDATE_RECEIPT_LINE_ITEM = 'UPDATE_RECEIPT_LINE_ITEM';
+
+export function setRemoteReceipts(remoteReceipts) {
+    console.log("setRemoteReceipts - action");
+    return (dispatch) => { dispatch({ type: SET_REMOTE_RECEIPTS, data: { remoteReceipts } }) };
+}
+
+export function addLocalReceipt(receipt) {
+    console.log('addLocalReceipt - action');
+    return dispatch => { dispatch({ type: ADD_LOCAL_RECEIPT, data: { receipt } }) };
+}
+
+export function setLocalReceipts(localReceipts) {
+    console.log('setLocalReceipts - action');
+    return dispatch => { dispatch({ type: SET_LOCAL_RECEIPTS, data: { localReceipts } }) };
+}
+
+export function updateRemoteReceipt(receiptIndex, updatedFields) {
+    console.log('updateRemoteReceipt - action');
+    return dispatch => {
+        dispatch({
+            type: UPDATE_REMOTE_RECEIPT,
+            data: {
+                remoteReceiptIndex: receiptIndex,
+                updatedRemoteFields: updatedFields
+            }
+        })
+    };
+}
+
+export function updateReceiptLineItem(receiptIndex, lineItemIndex, updatedFields) {
+    console.log('updateRemoteReceipt - action');
+    return dispatch => {
+        dispatch({
+            type: UPDATE_RECEIPT_LINE_ITEM,
+            data: {
+                receiptIndex,
+                lineItemIndex,
+                updatedLineItemFields: updatedFields
+            }
+        })
+    };
+}
+
+export function updateLocalReceipt(item, updatedFields) {
+    console.log('updateLocalReceipt - action');
+
+    PosStorage._loadPendingSale(item.id).then((sale) => {
+        return ({ key: item.id, sale });
+    })
+        .then(receipt => {
+            const saleData = {
+                key: item.id,
+                sale: {
+                    ...receipt.sale,
+                    ...updatedFields
+                }
+            }
+
+            PosStorage.updatePendingSale(item.id, saleData);
+        });
+
+    return dispatch => {
+        dispatch({
+            type: UPDATE_LOCAL_RECEIPT,
+            data: {
+                localReceiptIndex: item.index,
+                updatedLocalFields: updatedFields
+            }
+        })
+    };
+}
--- mobile_client/src/actions/SalesLoggingActions.js
+++
@@ -1,6 +0,0 @@
-export const SET_REMOTE_SALES = 'SET_REMOTE_SALES';
-
-export function setRemoteSales(remoteSales) {
-    console.log("SetRemoteSales - action");
-    return (dispatch) => { dispatch({ type: SET_REMOTE_SALES, data: { remoteSales } }) };
-}
