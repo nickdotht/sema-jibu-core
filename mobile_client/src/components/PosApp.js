9446db58ca070a560a9cdd2bdbf8637ef4779423
Nick Rameau <r@meau.me>
2019-01-10 11:13:20 -0500

    [Patch] Comment out unnecessary code

--- mobile_client/src/components/PosApp.js
+++ mobile_client/src/components/PosApp.js
@@ -105,7 +105,7 @@ class PosApp extends Component {
 		Events.on('SalesChannelsUpdated', 'SalesChannelsUpdated1', this.onSalesChannelUpdated.bind(this));
 		Events.on('UILanguageUpdated', 'UILanguageUpdated1', this.onLanguageUpdated.bind(this));
 		Events.on('ReceiptsFetched', 'ReceiptsFetched1', this.onReceiptsFetched.bind(this));
-		Events.on('NewSaleAdded', 'NewSaleAdded1', this.onNewSaleAdded.bind(this));
+		// Events.on('NewSaleAdded', 'NewSaleAdded1', this.onNewSaleAdded.bind(this));
 		// Events.on('LocalReceiptsUpdated', 'LocalReceiptsUpdated1', this.onLocalReceiptsUpdated.bind(this));
 		console.log("PosApp = Mounted-Done");
 
@@ -116,7 +116,7 @@ class PosApp extends Component {
 		Events.rm('SalesChannelsUpdated', 'SalesChannelsUpdated1');
 		Events.rm('UILanguageUpdated', 'UILanguageUpdated1');
 		Events.rm('ReceiptsFetched', 'ReceiptsFetched1');
-		Events.rm('NewSaleAdded', 'NewSaleAdded1');
+		// Events.rm('NewSaleAdded', 'NewSaleAdded1');
 		// Events.rm('LocalReceiptsUpdated', 'LocalReceiptsUpdated1');
 		NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
 	}
