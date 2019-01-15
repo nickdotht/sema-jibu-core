1847a41149d8fdc8c11b3c4c479e9eb6f25d6f94
Nick Rameau <r@meau.me>
2019-01-15 01:27:50 -0500

    [Refactor + Patch] Add function to insert single receipt into redux

--- mobile_client/src/actions/ReceiptActions.js
+++ mobile_client/src/actions/ReceiptActions.js
@@ -1,25 +1,31 @@
 import PosStorage from "../database/PosStorage";
 
 export const SET_REMOTE_RECEIPTS = 'SET_REMOTE_RECEIPTS';
-export const ADD_LOCAL_RECEIPT = 'ADD_LOCAL_RECEIPT';
+export const ADD_REMOTE_RECEIPT = 'ADD_REMOTE_RECEIPT';
 export const SET_LOCAL_RECEIPTS = 'SET_LOCAL_RECEIPTS';
 export const UPDATE_REMOTE_RECEIPT = 'UPDATE_REMOTE_RECEIPT';
 export const UPDATE_LOCAL_RECEIPT = 'UPDATE_LOCAL_RECEIPT';
 export const UPDATE_RECEIPT_LINE_ITEM = 'UPDATE_RECEIPT_LINE_ITEM';
+export const REMOVE_LOCAL_RECEIPT = 'REMOVE_LOCAL_RECEIPT';
 
 export function setRemoteReceipts(remoteReceipts) {
     console.log("setRemoteReceipts - action");
     return (dispatch) => { dispatch({ type: SET_REMOTE_RECEIPTS, data: { remoteReceipts } }) };
 }
 
-export function addLocalReceipt(receipt) {
-    console.log('addLocalReceipt - action');
-    return dispatch => { dispatch({ type: ADD_LOCAL_RECEIPT, data: { receipt } }) };
+export function addRemoteReceipt(receipt) {
+    console.log('addRemoteReceipt - action');
+    return dispatch => { dispatch({ type: ADD_REMOTE_RECEIPT, data: { receipt } }) };
 }
 
 export function setLocalReceipts(localReceipts) {
     console.log('setLocalReceipts - action');
     return dispatch => { dispatch({ type: SET_LOCAL_RECEIPTS, data: { localReceipts } }) };
+}
+
+export function removeLocalReceipt(receiptId) {
+    console.log(`removeLocalReceipt - action ${receiptId}`);
+    return dispatch => { dispatch({ type: REMOVE_LOCAL_RECEIPT, data: { receiptId } }) };
 }
 
 export function updateRemoteReceipt(receiptIndex, updatedFields) {
